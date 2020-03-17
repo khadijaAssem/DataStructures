cc76dbca97d60a39fb46806c1f721114acf9c440
khadijaAssem <42361430+khadijaAssem@users.noreply.github.com>
2020-03-17 03:16:56 +0200

    Add files via upload

---
+++ LinkedListss/LinkedListNode.java
@@ -0,0 +1,10 @@
+public class LinkedListNode {
+     double value;
+     LinkedListNode next;
+     public double getValue(){
+        return value;
+     }
+     public LinkedListNode getNext(){
+         return next;
+     }
+}
